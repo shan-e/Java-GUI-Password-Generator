798352635201c3c80e3fa703168e95cdd6f99053
shan-e <126803195+shan-e@users.noreply.github.com>
2024-02-09 02:04:33 +0000

    Main method with console interface TUI

--- GUI Password Generator/src/Main.java
+++ GUI Password Generator/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Utilities utilities = new Utilities();
+        RandomChars randomChars = new RandomChars();
+        PasswordOptions passwordOptions = new PasswordOptions(randomChars, utilities);
+
+        utilities.clearConsole();
+        System.out.print("Welcome to Password Generator\nWhat password length do you require: ");
+        Scanner scanner = new Scanner(System.in);
+
+        int passLength = scanner.nextInt();
+        char[] password = new char[passLength];
+        utilities.printOptions();
+        String choice = scanner.next();
+
+        passwordOptions.switchCase(choice.toCharArray(), password);
+        System.out.printf("Your newly generated password consisting of %d characters is:\n%s\n", password.length, new String(password));
     }
 }
