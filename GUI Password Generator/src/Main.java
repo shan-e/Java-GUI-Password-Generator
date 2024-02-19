a94169ca4b840af4b369b00a79d0354f4eb5a1e5
shan-e <126803195+shan-e@users.noreply.github.com>
2024-02-19 23:39:21 +0000

    class Main will now invoke the GUI using my implemented run method in PasswordOptions.java

--- GUI Password Generator/src/Main.java
+++ GUI Password Generator/src/Main.java
@@ -1,21 +1,6 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Utilities utilities = new Utilities();
-        RandomChars randomChars = new RandomChars();
-        PasswordOptions passwordOptions = new PasswordOptions(randomChars, utilities);
-
-        utilities.clearConsole();
-        System.out.print("Welcome to Password Generator\nWhat password length do you require: ");
-        Scanner scanner = new Scanner(System.in);
-
-        int passLength = scanner.nextInt();
-        char[] password = new char[passLength];
-        utilities.printOptions();
-        String choice = scanner.next();
-
-        passwordOptions.switchCase(choice.toCharArray(), password);
-        System.out.printf("Your newly generated password consisting of %d characters is:\n%s\n", password.length, new String(password));
+        PasswordGenerator passwordGenerator = new PasswordGenerator();
+        passwordGenerator.run();
     }
 }
