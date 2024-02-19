5bc420d8296084c4c280b5e16428d5c6643d1401
shan-e <126803195+shan-e@users.noreply.github.com>
2024-02-19 23:35:38 +0000

    classes not in use currently

--- GUI Password Generator/src/PasswordOptions.java
+++ GUI Password Generator/src/PasswordOptions.java
@@ -1,80 +1,80 @@
-import java.util.Random;
-
-public class PasswordOptions {
-    private final RandomChars randomChars;
-    private final Utilities utilities;
-
-    public PasswordOptions(RandomChars randomChars, Utilities utilities) {
-        this.randomChars = randomChars;
-        this.utilities = utilities;
-    }
-
-    public void optionA(char[] password) {
-        for (int i = 0; i < password.length; i++) password[i] = randomChars.randomLowerCase();
-    }
-
-    public void optionB(char[] password) {
-        for (int i = 0; i < password.length; i++) password[i] = randomChars.randomUpperCase();
-    }
-
-    public void optionC(char[] password) {
-        Random rand = new Random();
-        int typeSwitcher;
-        for (int i = 0; i < password.length; i++) {
-            typeSwitcher = rand.nextInt(2);
-            if (typeSwitcher == 0) password[i] = randomChars.randomUpperCase();
-            else password[i] = randomChars.randomLowerCase();
-        }
-    }
-
-    public void optionD(char[] password) {
-        Random rand = new Random();
-        int typeSwitcher;
-        for (int i = 0; i < password.length; i++) {
-            typeSwitcher = rand.nextInt(2);
-            if (typeSwitcher == 0) password[i] = randomChars.randomLowerCase();
-            else password[i] = randomChars.randomSpecialChar();
-        }
-    }
-
-    public void optionE(char[] password) {
-        Random rand = new Random();
-        int typeSwitcher;
-        for (int i = 0; i < password.length; i++) {
-            typeSwitcher = rand.nextInt(2);
-            if (typeSwitcher == 0) password[i] = randomChars.randomUpperCase();
-            else password[i] = randomChars.randomSpecialChar();
-        }
-    }
-
-    public void optionF(char[] password) {
-        for (int i = 0; i < password.length; i++) password[i] = randomChars.randomSpecialChar();
-    }
-
-    public void optionG(char[] password) {
-        Random rand = new Random();
-        int typeSwitcher;
-        for (int i = 0; i < password.length; i++) {
-            typeSwitcher = rand.nextInt(3);
-            if (typeSwitcher == 0) password[i] = randomChars.randomUpperCase();
-            else if (typeSwitcher == 1) password[i] = randomChars.randomSpecialChar();
-            else password[i] = randomChars.randomLowerCase();
-        }
-    }
-
-    public void switchCase(char[] choice, char[] password) {
-        System.out.println(choice[0]);
-        switch (Character.toUpperCase(choice[0])) {
-            case 'A': optionA(password); System.out.println("this one ran");break;
-            case 'B': optionB(password); break;
-            case 'C': optionC(password); break;
-            case 'D': optionD(password); break;
-            case 'E': optionE(password); break;
-            case 'F': optionF(password); break;
-            case 'G': optionG(password); break;
-            default: utilities.printSwitchCaseErrorMsg(); break;
-        }
-    }
-}
-
-
+//import java.util.Random;
+//
+//public class PasswordOptions {
+//    private final RandomChars randomChars;
+//    private final Utilities utilities;
+//
+//    public PasswordOptions(RandomChars randomChars, Utilities utilities) {
+//        this.randomChars = randomChars;
+//        this.utilities = utilities;
+//    }
+//
+//    public void optionA(char[] password) {
+//        for (int i = 0; i < password.length; i++) password[i] = randomChars.randomLowerCase();
+//    }
+//
+//    public void optionB(char[] password) {
+//        for (int i = 0; i < password.length; i++) password[i] = randomChars.randomUpperCase();
+//    }
+//
+//    public void optionC(char[] password) {
+//        Random rand = new Random();
+//        int typeSwitcher;
+//        for (int i = 0; i < password.length; i++) {
+//            typeSwitcher = rand.nextInt(2);
+//            if (typeSwitcher == 0) password[i] = randomChars.randomUpperCase();
+//            else password[i] = randomChars.randomLowerCase();
+//        }
+//    }
+//
+//    public void optionD(char[] password) {
+//        Random rand = new Random();
+//        int typeSwitcher;
+//        for (int i = 0; i < password.length; i++) {
+//            typeSwitcher = rand.nextInt(2);
+//            if (typeSwitcher == 0) password[i] = randomChars.randomLowerCase();
+//            else password[i] = randomChars.randomSpecialChar();
+//        }
+//    }
+//
+//    public void optionE(char[] password) {
+//        Random rand = new Random();
+//        int typeSwitcher;
+//        for (int i = 0; i < password.length; i++) {
+//            typeSwitcher = rand.nextInt(2);
+//            if (typeSwitcher == 0) password[i] = randomChars.randomUpperCase();
+//            else password[i] = randomChars.randomSpecialChar();
+//        }
+//    }
+//
+//    public void optionF(char[] password) {
+//        for (int i = 0; i < password.length; i++) password[i] = randomChars.randomSpecialChar();
+//    }
+//
+//    public void optionG(char[] password) {
+//        Random rand = new Random();
+//        int typeSwitcher;
+//        for (int i = 0; i < password.length; i++) {
+//            typeSwitcher = rand.nextInt(3);
+//            if (typeSwitcher == 0) password[i] = randomChars.randomUpperCase();
+//            else if (typeSwitcher == 1) password[i] = randomChars.randomSpecialChar();
+//            else password[i] = randomChars.randomLowerCase();
+//        }
+//    }
+//
+//    public void switchCase(char[] choice, char[] password) {
+//        System.out.println(choice[0]);
+//        switch (Character.toUpperCase(choice[0])) {
+//            case 'A': optionA(password); System.out.println("this one ran");break;
+//            case 'B': optionB(password); break;
+//            case 'C': optionC(password); break;
+//            case 'D': optionD(password); break;
+//            case 'E': optionE(password); break;
+//            case 'F': optionF(password); break;
+//            case 'G': optionG(password); break;
+//            default: utilities.printSwitchCaseErrorMsg(); break;
+//        }
+//    }
+//}
+//
+//
--- GUI Password Generator/src/RandomChars.java
+++ GUI Password Generator/src/RandomChars.java
@@ -1,20 +1,14 @@
-import java.util.Random;
-
-public class RandomChars {
-    public char randomLowerCase() {
-        return (char) (97 + new Random().nextInt(26));
-    }
-
-    public char randomUpperCase() {
-        return (char) (65 + new Random().nextInt(26));
-    }
-
-    public char randomSpecialChar() {
-        Random rand = new Random();
-        int charCategory = rand.nextInt(4);
-        if (charCategory == 0) return (char) (33 + rand.nextInt(15));
-        else if (charCategory == 1) return (char) (58 + rand.nextInt(7));
-        else if (charCategory == 2) return (char) (91 + rand.nextInt(6));
-        else return (char) (123 + rand.nextInt(4));
-    }
-}
+//import java.util.Random;
+//
+//public class RandomChars {
+//    public char randomChar() {
+//        Random rand = new Random();
+//        int charCategory = rand.nextInt(6);
+//        if (charCategory == 0) return (char) (33 + rand.nextInt(15));
+//        else if (charCategory == 1) return (char) (58 + rand.nextInt(7));
+//        else if (charCategory == 2) return (char) (91 + rand.nextInt(6));
+//        else if (charCategory == 3) return (char) (97 + new Random().nextInt(26));
+//        else if (charCategory == 4) return (char) (65 + new Random().nextInt(26));
+//        else return (char) (123 + rand.nextInt(4));
+//    }
+//}
