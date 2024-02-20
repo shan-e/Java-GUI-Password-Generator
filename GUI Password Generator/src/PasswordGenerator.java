f3c998160ff9e44ffac084fe00efff4be1a8fe98
shan-e <126803195+shan-e@users.noreply.github.com>
2024-02-20 17:46:59 +0000

    set variables as private instance variables

--- GUI Password Generator/src/PasswordGenerator.java
+++ GUI Password Generator/src/PasswordGenerator.java
@@ -1,12 +1,19 @@
 import javax.swing.*;
 
 // info window about password stats
+// have type of password chooser
 
-public class PasswordGenerator implements Runnable{
-    static int value;
-    static String password;
-
+public class PasswordGenerator implements Runnable {
+    private int value;
+    private String password;
     private final JFrame jFrame;
+    private final JPanel jPanel;
+    private final JLabel welcomingTheUser;
+    private final JLabel detailedInstructions;
+    private final JSlider slider;
+    private final JLabel finalPasswordTextOutput;
+    private final JButton generatePasswordButton;
+    private final JButton exitTheGUIBruh;
 
     public PasswordGenerator() {
         jFrame = new JFrame("My GUI Password Generator :)");
@@ -17,21 +24,21 @@ public class PasswordGenerator implements Runnable{
         jFrame.setIconImage(new ImageIcon("/Users/shane/Desktop/Projects/Java-GUI-Password-Generator/GUI Password Generator/src/421648.png").getImage());
 
 
-        JPanel jPanel = new JPanel(); // Change JLabel to JPanel
+        jPanel = new JPanel(); // Change JLabel to JPanel
         jPanel.setLayout(null); // You are using absolute positioning, so set layout to null
         jPanel.setSize(854, 480);
 
-        JLabel welcomingTheUser = new JLabel("Welcome to Password Generator");
+        welcomingTheUser = new JLabel("Welcome to Password Generator");
         welcomingTheUser.setHorizontalAlignment(SwingConstants.CENTER);
         welcomingTheUser.setBounds(277, 50, 300, 25);
         jPanel.add(welcomingTheUser);
 
-        JLabel detailedInstructions = new JLabel("Please use the slider to choose password length");
+        detailedInstructions = new JLabel("Please use the slider to choose password length");
         detailedInstructions.setHorizontalAlignment(SwingConstants.CENTER);
         detailedInstructions.setBounds(227, 75, 400, 25);
         jPanel.add(detailedInstructions);
 
-        JSlider slider = new JSlider(JSlider.HORIZONTAL, 10, 50, 32); // min, max, initial value
+        slider = new JSlider(JSlider.HORIZONTAL, 10, 50, 32); // min, max, initial value
         slider.setMinorTickSpacing(2); // spacing between ticks
         slider.setMajorTickSpacing(10);
         slider.setPaintTicks(true); // paint ticks
@@ -41,7 +48,7 @@ public class PasswordGenerator implements Runnable{
 
         value = slider.getValue();
 
-        JLabel finalPasswordTextOutput = new JLabel("Press Generate to create a random unique password consisting of " + value + " characters");
+        finalPasswordTextOutput = new JLabel("Press Generate to create a random unique password consisting of " + value + " characters");
         finalPasswordTextOutput.setHorizontalAlignment(SwingConstants.CENTER);
         finalPasswordTextOutput.setBounds(52, 260, 750, 25);
         slider.addChangeListener(e -> {
@@ -50,17 +57,17 @@ public class PasswordGenerator implements Runnable{
         });
         jPanel.add(finalPasswordTextOutput);
 
-        JButton generatePasswordButton = new JButton("Generate");
+        generatePasswordButton = new JButton("Generate");
         generatePasswordButton.setBounds(377, 200, 100, 50);
         generatePasswordButton.addActionListener(e ->
         {
             password = Utilities.generatePassword(value);
             finalPasswordTextOutput.setText("Your unique password consisting of " + value + " characters is " + password);
-            System.out.println("Generating new password, hope the password is secure enough (You're not meant to see this - run while you still can!!?!)");
+            System.out.println("Generating new password, hope the password is secure enough (You're not meant to see this - run while you still can!!?!) BOOO!");
         });
         jPanel.add(generatePasswordButton);
 
-        JButton exitTheGUIBruh = new JButton("Exit");
+        exitTheGUIBruh = new JButton("Exit");
         exitTheGUIBruh.setBounds(377, 290, 100, 50);
         exitTheGUIBruh.addActionListener(e -> {
             System.out.println("Button clicked (exit)");
