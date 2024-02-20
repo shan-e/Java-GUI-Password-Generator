d3ffc3f34f2059560b5c99c88834b626dbd8958f
shan-e <126803195+shan-e@users.noreply.github.com>
2024-02-20 22:51:41 +0000

    added authorship

--- GUI Password Generator/src/Main.java
+++ GUI Password Generator/src/Main.java
@@ -1,3 +1,14 @@
+/**
+ * Author:  Shane Easo
+ * Email:   shane.easo@ucdconnect.ie
+ * GitHub:  shan-e
+ * Created:   08.02.2024
+ * Description:
+ * This is a small project I had been working on in my spare time.
+ * It is a GUI interface for a very basic password generator.
+ * Resources were obtained from my lecture notes and Oracle documentation
+ **/
+
 public class Main {
     public static void main(String[] args) {
         PasswordGenerator passwordGenerator = new PasswordGenerator(); // creating an instance of the PasswordGenerator class
