d6a36f9cb59483f057f2c4d6a58713cc7d541475
shan-e <126803195+shan-e@users.noreply.github.com>
2024-03-07 00:08:14 +0000

    integrated option to choose specific password layouts options using JComboBox and copy to clipboard feature

--- GUI Password Generator/src/PasswordGenerator.java
+++ GUI Password Generator/src/PasswordGenerator.java
@@ -10,15 +10,23 @@
  **/
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 // info window about password stats
 // have type of password chooser
 // regions with numerical values will be measured in pixels
 
-public class PasswordGenerator implements Runnable {
+public class PasswordGenerator implements Runnable, ActionListener {
     // implementation below allows GUI to run properly
     // when this implemented run function is run, the JFrame's visibility
     // property is set to true, hence displaying the app window
-    @Override public void run() { jFrame.setVisible(true); }
+    @Override
+    public void run() {
+        jFrame.setVisible(true);
+    }
 
     // my instance variables, which are later constructed
     // all variables are private to allow for class accessibility
@@ -29,10 +37,13 @@ public class PasswordGenerator implements Runnable {
     private final JPanel jPanel; // layout within the app window
     private final JLabel welcomingTheUser; // greets the user
     private final JLabel detailedInstructions; // tells user how to use the app
+    private final JComboBox passwordOptions;
     private final JSlider slider; // JSlider that controls the password's length
     private final JLabel finalPasswordTextOutput;// gives the formatted "toString" containing the new password
     private final JButton generatePasswordButton; // when pressed generates a new unique password
     private final JButton exitTheGUIBruh; // exits the window and entire program
+    private final JButton copyToClipboard;
+    private static int option;
 
     public PasswordGenerator() {
         jFrame = new JFrame("My GUI Password Generator :)"); // window title
@@ -62,6 +73,23 @@ public class PasswordGenerator implements Runnable {
         detailedInstructions.setBounds(227, 75, 400, 25);
         jPanel.add(detailedInstructions); // adds this label to the panel
 
+        String[] options = {
+                "Option 1 - Lower Case",
+                "Option 2 - Upper Case",
+                "Option 3 - Upper & Lower Case",
+                "Option 4 - Lower & Special Characters",
+                "Option 5 - Upper & Special Characters",
+                "Option 6 - Special Characters",
+                "Option 7 - All of the above"
+        };
+        passwordOptions = new JComboBox<>(options);
+        passwordOptions.setBounds(277, 90, 300, 75);
+        passwordOptions.setSelectedIndex(2);
+        option = 2;
+        passwordOptions.addActionListener(e -> option = passwordOptions.getSelectedIndex());
+        jPanel.add(passwordOptions);
+
+
         slider = new JSlider(JSlider.HORIZONTAL, 10, 50, 32); // slider with 10-50 range initially at 32
         slider.setMinorTickSpacing(2); // for every 2nd value, there will be a minor tick
         slider.setMajorTickSpacing(10); // for every 10th value, there will be a major tick
@@ -69,7 +97,7 @@ public class PasswordGenerator implements Runnable {
         slider.setPaintLabels(true); // allows the numerical checkpoints to show
         // sets region where slider should be placed **in pixels**
         // this slider is 300x100 with the top-left corner going across 227 and 100 down
-        slider.setBounds(277, 100, 300, 100);
+        slider.setBounds(277, 130, 300, 100);
         jPanel.add(slider); // adds slider to the panel
 
         // whatever value the slider is on (32 now, due to initial), save that as the password length
@@ -82,7 +110,7 @@ public class PasswordGenerator implements Runnable {
         finalPasswordTextOutput.setHorizontalAlignment(SwingConstants.CENTER); // centres label according to the width of the label
         // sets region where label should be placed **in pixels**
         // this label is 750x25 with the top-left corner going across 52 and 260 down
-        finalPasswordTextOutput.setBounds(52, 260, 750, 25);
+        finalPasswordTextOutput.setBounds(52, 285, 750, 25);
         slider.addChangeListener(e -> {
             // lambda function which detects change in the slider values
             // whatever the new password length value is, update that as the password length
@@ -95,26 +123,44 @@ public class PasswordGenerator implements Runnable {
         generatePasswordButton = new JButton("Generate"); // generate button
         // sets region where label should be placed **in pixels**
         // this label is 100x50 with the top-left corner going across 377 and 200 down
-        generatePasswordButton.setBounds(377, 200, 100, 50);
+        generatePasswordButton.setBounds(377, 220, 100, 50);
         // this lambda expression detects whether the generate button is pressed or not
         generatePasswordButton.addActionListener(e -> {
             // if pressed, let a new password be generated based on the current password length
             // this int will run through generatePassword and be saved to the password String
-            password = Utilities.generatePassword(passwordLength);
+            password = Utilities.generatePassword(passwordLength, option);
             // change the output text to contain the newly generated password
             finalPasswordTextOutput.setText("Your unique password consisting of " + passwordLength + " characters is " + password);
-            System.out.println("Generating new password, hope the password is secure enough (You're not meant to see this - run while you still can!!?!) BOOO!"); // hee-hee
+            System.out.println("Generating new password, hope the password is secure enough (You're not meant to see this - run while you still can!!?!)"); // hee-hee
         });
         jPanel.add(generatePasswordButton); // add the generate button to the JPanel
 
         exitTheGUIBruh = new JButton("Exit"); // Exit button to terminate program
         // sets region where label should be placed **in pixels**
         // this label is 100x50 with the top-left corner going across 377 and 290 down
-        exitTheGUIBruh.setBounds(377, 290, 100, 50);
+        exitTheGUIBruh.setBounds(377, 330, 100, 50);
         // lambda expression to check when pressed, if-so, exit the program with status 0.
         exitTheGUIBruh.addActionListener(e -> System.exit(0));
         jPanel.add(exitTheGUIBruh);// add the button to the panel
 
+        copyToClipboard = new JButton("Copy"); // Exit button to terminate program
+        // sets region where label should be placed **in pixels**
+        // this label is 100x50 with the top-left corner going across 377 and 290 down
+        copyToClipboard.setBounds(377, 370, 100, 50);
+        // lambda expression to check when pressed, if-so, exit the program with status 0.
+        copyToClipboard.addActionListener(e -> {
+            StringSelection stringSelection = new StringSelection(String.valueOf(password));
+            Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+            clipboard.setContents(stringSelection, null);
+        });
+        jPanel.add(copyToClipboard);// add the button to the panel
+
+
         jFrame.add(jPanel); // add the JPanel to the JFrame to display its contents
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == passwordOptions) System.out.println("hey");
     }
 }
