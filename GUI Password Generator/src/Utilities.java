6df2413c8523f4747cf832851f8c68313be20a3b
shan-e <126803195+shan-e@users.noreply.github.com>
2024-05-08 18:19:48 +0100

    removed unnecessary comments like i aint readin all that

--- GUI Password Generator/src/Utilities.java
+++ GUI Password Generator/src/Utilities.java
@@ -1,62 +1,46 @@
 import java.util.Random;
 
 public class Utilities {
-    private static final Random random = new Random(); // global random object instance that can be used anywhere within the class
+    private static final Random random = new Random();
 
-    public static char generateAnyPasswordCharacter() {
-        // charCategory dictates which section/region of the ASCII table to use to generate a random char
-        int charCategory = random.nextInt(3);
-        // if the random number matches any of the conditions, it will generate a random number
-        // between the given bounds and cast the int as a char to finally output a random character
-        if (charCategory == 0) return generateLowerCaseCharacter();
-        else if (charCategory == 1) return generateUpperCaseCharacter();
+    private static char generateAnyPasswordCharacter() {
+        int pickOption = random.nextInt(3);
+        if (pickOption == 0) return generateLowerCaseCharacter();
+        else if (pickOption == 1) return generateUpperCaseCharacter();
         else return generateSpecialCharacter();
     }
-    public static char generateUpperCaseCharacter() { return (char) (65 + random.nextInt(26)); } // characters A-Z
-    public static char generateLowerCaseCharacter() { return (char) (97 + random.nextInt(26)); } // characters a-z
+    private static char generateUpperCaseCharacter() { return (char) (65 + random.nextInt(26)); } // characters A-Z
+    private static char generateLowerCaseCharacter() { return (char) (97 + random.nextInt(26)); } // characters a-z
 
-    public static char generateSpecialCharacter() {
-        // charCategory dictates which section/region of the ASCII table to use to generate a random char
-        int charCategory = random.nextInt(4);
-        // if the random number matches any of the conditions, it will generate a random number
-        // between the given bounds and cast the int as a char to finally output a random character
-        if (charCategory == 0) return (char) (33 + random.nextInt(15)); // characters ! " # $ % & ' ( ) * + , - . /
-        else if (charCategory == 1) return (char) (58 + random.nextInt(7)); // characters : ; < = > ? @
-        else if (charCategory == 2) return (char) (91 + random.nextInt(6)); // characters [ \ ] ^ _ \
+    private static char generateSpecialCharacter() {
+        int pickOption = random.nextInt(4);
+        if (pickOption == 0) return (char) (33 + random.nextInt(15)); // characters ! " # $ % & ' ( ) * + , - . /
+        else if (pickOption == 1) return (char) (58 + random.nextInt(7)); // characters : ; < = > ? @
+        else if (pickOption == 2) return (char) (91 + random.nextInt(6)); // characters [ \ ] ^ _ \
         else return (char) (123 + random.nextInt(4));  // characters { | } ~
     }
 
-    public static char generateUpperOrLowerCharacter() {
-        // charCategory dictates which section/region of the ASCII table to use to generate a random char
-        int charCategory = random.nextInt(2);
-        // if the random number matches any of the conditions, it will generate a random number
-        // between the given bounds and cast the int as a char to finally output a random character
-        if (charCategory == 0) return generateLowerCaseCharacter();
+    private static char generateUpperOrLowerCharacter() {
+        int pickOption = random.nextInt(2);
+        if (pickOption == 0) return generateLowerCaseCharacter();
         else return generateUpperCaseCharacter();
     }
 
-    public static char generateUpperOrSpecialCharacter() {
-        // charCategory dictates which section/region of the ASCII table to use to generate a random char
-        int charCategory = random.nextInt(2);
-        // if the random number matches any of the conditions, it will generate a random number
-        // between the given bounds and cast the int as a char to finally output a random character
-        if (charCategory == 0) return generateSpecialCharacter();
+    private static char generateUpperOrSpecialCharacter() {
+        int pickOption = random.nextInt(2);
+        if (pickOption == 0) return generateSpecialCharacter();
         else return generateUpperCaseCharacter();
     }
 
-    public static char generateLowerOrSpecialCharacter() {
-        // charCategory dictates which section/region of the ASCII table to use to generate a random char
-        int charCategory = random.nextInt(2);
-        // if the random number matches any of the conditions, it will generate a random number
-        // between the given bounds and cast the int as a char to finally output a random character
-        if (charCategory == 0) return generateSpecialCharacter();
+    private static char generateLowerOrSpecialCharacter() {
+        int pickOption = random.nextInt(2);
+        if (pickOption == 0) return generateSpecialCharacter();
         else return generateLowerCaseCharacter();
     }
 
     public static String generatePassword(int passwordLength, int option) {
-        // creating a char array to store a random ASCII character, given the length required for the password
-        char[] passwordAsCharArray = new char[passwordLength]; // password first a char array to insert char at every increment
-        // switch case dictates which option should be run from combo-box
+        // password as a char array first to insert char at every increment
+        char[] passwordAsCharArray = new char[passwordLength];
         switch (option) {
             case 0: for (int i = 0; i < passwordLength; i++) passwordAsCharArray[i] = generateLowerCaseCharacter(); break;
             case 1: for (int i = 0; i < passwordLength; i++) passwordAsCharArray[i] = generateUpperCaseCharacter(); break;
